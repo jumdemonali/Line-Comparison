a40580706f1b71f7f2925984a5db39c121ce05b0
jumdemonali <jumdemonali@gmail.com>
2021-08-15 17:22:56 +0530

    Line comparision example

--- LineCalculation.java
+++ LineCalculation.java
@@ -5,16 +5,16 @@ public class LineCalculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double x1 = 4 , y1 = 5 , x2 = 5 , y2 = 1 , x3 = 5 , y3 = 8 , x4 = 8 , y4 = 1 ;
-		double dist = Math.sqrt(Math.pow(x2 - x1 , 2) + Math.pow(y2- y1 , 2));
-	    double dist1 = Math.sqrt(Math.pow(x4 - x3 , 2) + Math.pow(y4- y3 , 2));
-	    String line1 = Double.toString(dist);
-	    String line2 = Double.toString(dist1);
+		double distance = Math.sqrt(Math.pow(x2 - x1 , 2) + Math.pow(y2- y1 , 2));
+	    double distance1 = Math.sqrt(Math.pow(x4 - x3 , 2) + Math.pow(y4- y3 , 2));
+	    String line1 = Double.toString(distance);
+	    String line2 = Double.toString(distance1);
 	    Boolean equal = line1.equals(line2);
-	    int compare = line1.compareTo(line2);
-	    if ( compare == 0 ) {
+	    int comp = line1.compareTo(line2);
+	    if ( comp == 0 ) {
 	    	System.out.println("Both Lines are equal");
 	    }
-	    else if ( compare >= 1)
+	    else if ( comp >= 1)
 		{
 			System.out.println("Line1 greater then other line ");
 		}
