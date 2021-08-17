916ba8ca82b32100eb83e93730d2e749f0644b02
jumdemonali <60069456+jumdemonali@users.noreply.github.com>
2021-08-17 13:08:09 +0530

    Update LineCalculation.java

--- LineCalculation.java
+++ LineCalculation.java
@@ -3,9 +3,8 @@ package com.bl.pract;
 public class LineCalculation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		double x1 = 4 , y1 = 5 , x2 = 5 , y2 = 1 , x3 = 5 , y3 = 8 , x4 = 8 , y4 = 1 ;
-		double distance = Math.sqrt(Math.pow(x2 - x1 , 2) + Math.pow(y2- y1 , 2));
+	    double x1 = 4 , y1 = 5 , x2 = 5 , y2 = 1 , x3 = 5 , y3 = 8 , x4 = 8 , y4 = 1 ;
+	    double distance = Math.sqrt(Math.pow(x2 - x1 , 2) + Math.pow(y2- y1 , 2));
 	    double distance1 = Math.sqrt(Math.pow(x4 - x3 , 2) + Math.pow(y4- y3 , 2));
 	    String line1 = Double.toString(distance);
 	    String line2 = Double.toString(distance1);
