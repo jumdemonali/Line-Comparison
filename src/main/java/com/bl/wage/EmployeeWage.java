31d416449a75c25e28e9557201ddb1380a6df316
jumdemonali <jumdemonali@gmail.com>
2021-08-25 19:50:03 +0530

    Added: Wage based on condition

--- src/main/java/com/bl/wage/EmployeeWage.java
+++ src/main/java/com/bl/wage/EmployeeWage.java
@@ -6,12 +6,13 @@ public class EmployeeWage
     public static final int IS_PART_TIME=1;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_HRS_IN_MONTH=100;
     public static void main(String[] args)
     {
-        int empHrs=0;
-        int empWage=0,totalEmpWage=0;
-        for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+        int empWage=0,totalEmpWage=0,empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             double empCheck=Math.floor(Math.random()*10)%3;
             switch((int)empCheck)
             {
@@ -25,11 +26,10 @@ public class EmployeeWage
                     empHrs=0;
                     break;
             }
+            totalEmpHrs+=empHrs;
             empWage=empHrs*EMP_RATE_PER_HOUR;
             totalEmpWage+=empWage;
-            System.out.println("Employee wage is "+empWage);
         }
-        System.out.println("Total Emp Wage: "+totalEmpWage);
+        System.out.println("Total Employee Wage: "+totalEmpWage);
     }
-
 }
