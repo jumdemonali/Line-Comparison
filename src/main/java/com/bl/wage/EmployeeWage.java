47a245f7916fee9c9613e11def23426731b66d2d
jumdemonali <jumdemonali@gmail.com>
2021-08-25 18:40:44 +0530

    Added: Employee Attendance

--- src/main/java/com/bl/wage/EmployeeWage.java
+++ src/main/java/com/bl/wage/EmployeeWage.java
@@ -4,5 +4,10 @@ public class EmployeeWage {
     public static void main(String args[])
     {
         System.out.println("Welcome to employee Wage");
+        double empCheck=Math.floor(Math.random()*10)%2;
+        if(empCheck == 1)
+            System.out.println("Employee is present ");
+        else
+            System.out.println("Employee is absent ");
     }
 }
