fe7e4b01d87a4e6d1c139b069e8b9dd7cabdc359
jumdemonali <jumdemonali@gmail.com>
2021-08-25 19:04:21 +0530

    Added: Daily Employee Wage

--- src/main/java/com/bl/wage/EmployeeWage.java
+++ src/main/java/com/bl/wage/EmployeeWage.java
@@ -1,13 +1,24 @@
 package com.bl.wage;
 
-public class EmployeeWage {
+public class EmployeeWage
+{
     public static void main(String args[])
     {
+        int WAGE_PER_HR=20;
+        int FULL_DAY_HR=8;
+        int wage;
         System.out.println("Welcome to employee Wage");
         double empCheck=Math.floor(Math.random()*10)%2;
-        if(empCheck == 1)
+        if(empCheck == 1) {
+            FULL_DAY_HR=8;
             System.out.println("Employee is present ");
+        }
         else
+        {
+            FULL_DAY_HR=0;
             System.out.println("Employee is absent ");
+        }
+        wage =WAGE_PER_HR*FULL_DAY_HR;
+        System.out.println("Employee wage is "+wage);
     }
 }
