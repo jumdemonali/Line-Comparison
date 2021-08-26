e13daeed644146083743462aa88deca97da0e823
jumdemonali <jumdemonali@gmail.com>
2021-08-26 13:31:15 +0530

    Added: Line Comparision Example

---
+++ src/main/java/com/bl/wage/LineCalculation.java
@@ -0,0 +1,23 @@
+package com.bl.wage;
+
+public class LineCalculation {
+    public static void main(String[] args) {
+        double x1 = 4 , y1 = 5 , x2 = 5 , y2 = 1 , x3 = 5 , y3 = 8 , x4 = 8 , y4 = 1 ;
+        double distance = Math.sqrt(Math.pow(x2 - x1 , 2) + Math.pow(y2- y1 , 2));
+        double distance1 = Math.sqrt(Math.pow(x4 - x3 , 2) + Math.pow(y4- y3 , 2));
+        String line1 = Double.toString(distance);
+        String line2 = Double.toString(distance1);
+        int comp = line1.compareTo(line2);
+        if ( comp == 0 ) {
+            System.out.println("Both Lines are equal");
+        }
+        else if ( comp >= 1)
+        {
+            System.out.println("Line1 greater then other line ");
+        }
+        else {
+            System.out.println("Line1 smaller then other line ");
+        }
+    }
+
+}
